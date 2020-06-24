4a38a2577f53715428a04cab0fee661e00055e87
RionC <67234765+RionC@users.noreply.github.com>
2020-06-24 13:11:47 -0600

    Final inheritance

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -11,7 +11,8 @@ namespace BankingDomain
             return _currentBalance;
         }
 
-        public void Deposit(decimal amountToDeposit)
+        // Overridable
+        public virtual void Deposit(decimal amountToDeposit)
         {
             _currentBalance += amountToDeposit;
         }
---
+++ BankingTests/GoldAccountDeposits.cs
@@ -0,0 +1,23 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldAccountDeposits
+    {
+        [Fact]
+        public void GoldAccountsGetABonusForDeposits()
+        {
+            var account = new GoldAccount();
+            var openingBalance = account.GetBalance();
+
+            account.Deposit(100M);
+
+            var expectedBalance = 110M + openingBalance;
+            Assert.Equal(expectedBalance, account.GetBalance());
+        }
+    }
+}
